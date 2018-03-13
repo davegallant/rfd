e5f2dd8bcbc4cd62bf0d83f767e5b52f44804b72
Dave Gallant <davegallant@gmail.com>
2018-03-12 23:59:31 -0400

    Fix count bugs (#1)
    * tail/head working
    * check if votes is null.

--- rfd/api.py
+++ rfd/api.py
@@ -67,7 +67,16 @@ def get_threads(forum_id, limit):
     return threads[:limit]
 
 
-def get_posts(post, limit=5):
+def get_posts(post, count=5, tail=False, per_page=40):
+    """Retrieve posts from a thread.
+
+    Args:
+        post (str): either post id or full url
+        count (int, optional): Description
+
+    Yields:
+        list(dict): body, score, and user
+    """
     if is_valid_url(post):
         post_id = extract_post_id(post)
     elif is_int(post):
@@ -75,28 +84,62 @@ def get_posts(post, limit=5):
     else:
         raise ValueError()
 
-    if limit == 0:
-        response = requests.get(
-            "https://forums.redflagdeals.com/api/topics/{}/posts?per_page=40&page=1".format(post_id))
-        pages = response.json().get('pager').get('total_pages')
-    elif limit > 40:
-        pages = ceil(limit / 40)
+    response = requests.get(
+        "https://forums.redflagdeals.com/api/topics/{}/posts?per_page=40&page=1".format(post_id))
+    total_posts = response.json().get('pager').get('total')
+    total_pages = response.json().get('pager').get('total_pages')
+
+    if count == 0:
+        pages = total_pages
+    if count > per_page:
+        if count > total_posts:
+            count = total_posts
+        pages = ceil(count / per_page)
     else:
-        pages = 1
+        if tail:
+            pages = total_pages
+        else:
+            pages = 1
+
+    if tail:
+        start_page = ceil((total_posts + 1 - count) / per_page)
+        start_post = (total_posts + 1 - count) % per_page
+        if start_post == 0:
+            start_post = per_page
+    else:
+        start_page, start_post = 0, 0
 
     # Go through as many pages as necessary
-    for page in range(pages):
-        page = page + 1  # page 0 causes issues
+    for page in range(start_page, pages + 1):
         response = requests.get(
             "https://forums.redflagdeals.com/api/topics/{}/posts?per_page={}&page={}".format(post_id,
                                                                                              get_safe_per_page(
-                                                                                                 limit),
+                                                                                                 per_page),
                                                                                              page))
 
         users = users_to_dict(response.json().get('users'))
-        for _post in response.json().get('posts'):
+
+        _posts = response.json().get('posts')
+
+        # Determine which post to start with (for --tail)
+        if page == start_page and not start_post == 0:
+            if tail:
+                _posts = _posts[start_post - 1:]
+            else:
+                _posts = _posts[:start_post]
+
+        for _post in _posts:
+            count -= 1
+            if count < 0:
+                return
+            # Sometimes votes is null
+            if _post.get('votes') is not None:
+                calculated_score = get_vote_score(_post.get('votes').get(
+                    'total_up'), _post.get('votes').get('total_down'))
+            else:
+                calculated_score = 0
             yield{
                 'body': strip_html(_post.get('body')),
-                'score': get_vote_score(_post.get('votes').get('total_up'), _post.get('votes').get('total_down')),
+                'score': calculated_score,
                 'user': users[_post.get('author_id')],
             }
---
+++ setup.py
@@ -0,0 +1,36 @@
+import io  # for python2
+from os import path
+from setuptools import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rfd',
+    version='0.0.5',
+    packages=['rfd'],
+    keywords='cli redflagdeals',
+    install_requires=[
+        'PyYAML>=3.11',
+        'beautifulsoup4>=4.6.0',
+        'click>=6.7',
+        'colorama>=0.3.9',
+        'pytest>=3.4.2',
+        'requests>=2.18.4'
+    ],
+    url='https://github.com/davegallant/rfd_cli',
+    license='Apache License, Version 2.0',
+    author='Dave Gallant',
+    author_email='davegallant@gmail.com',
+    description='CLI for RedFlagDeals.com',
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'rfd = rfd.rfd_cli:cli',
+        ],
+    },
+)
