7225d84f22a64f414426032780bf688365bae7a0
Dave Gallant <davegallant@gmail.com>
2018-04-08 21:01:14 -0400

    autogen requirements. (#4)
    * autogen requirements.txt

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
 import io  # for python2
 from os import path
 from setuptools import setup
+from pip.req import parse_requirements
 from version import __version__ as version
 
+WORKING_DIR = path.abspath(path.dirname(__file__))
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+# Get long description from reStructuredTExt
+with io.open(path.join(WORKING_DIR, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# read requirements.txt and load into list
+REQUIREMENTS_TXT = parse_requirements(path.join(WORKING_DIR, "requirements.txt"), session='my_session')
+REQUIREMENTS = [str(r.req) for r in REQUIREMENTS_TXT]
+
 setup(
-    name='rfd',
-    version=version,
-    packages=['rfd'],
-    keywords='cli redflagdeals',
-    install_requires=[
-        'PyYAML>=3.11',
-        'beautifulsoup4>=4.6.0',
-        'click>=6.7',
-        'colorama>=0.3.9',
-        'requests>=2.18.4'
-    ],
-    url='https://github.com/davegallant/rfd_cli',
-    license='Apache License, Version 2.0',
     author='Dave Gallant',
     author_email='davegallant@gmail.com',
     description='CLI for RedFlagDeals.com',
-    long_description=long_description,
     entry_points={
         'console_scripts': [
             'rfd = rfd.rfd_cli:cli',
-        ],
+            ],
     },
+    install_requires=REQUIREMENTS,
+    keywords='cli redflagdeals',
+    license='Apache License, Version 2.0',
+    long_description=long_description,
+    name='rfd',
+    packages=['rfd'],
+    url='https://github.com/davegallant/rfd_cli',
+    version=version,
 )
