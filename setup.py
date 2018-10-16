899506b462dd76b490d2b36fab56e26083649731
Dave Gallant <davegallant@gmail.com>
2018-10-16 16:49:47 -0400

    remove requirements.txt - https://caremad.io/posts/2013/07/setup-vs-requirement/

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 import io  # for python2
 from os import path
 from setuptools import setup, find_packages
-
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
 from rfd.__version__ import version
 
 WORKING_DIR = path.abspath(path.dirname(__file__))
@@ -14,18 +9,18 @@ WORKING_DIR = path.abspath(path.dirname(__file__))
 with io.open(path.join(WORKING_DIR, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-# read requirements.txt and load into list
-REQUIREMENTS_TXT = parse_requirements(
-    path.join(WORKING_DIR, "requirements.txt"), session="my_session"
-)
-REQUIREMENTS = [str(r.req) for r in REQUIREMENTS_TXT]
 
 setup(
     author="Dave Gallant",
-    author_email="davegallant@gmail.com",
+    author_email="davegallant at gmail",
     description="CLI for RedFlagDeals.com",
     entry_points={"console_scripts": ["rfd = rfd.rfd_cli:cli"]},
-    install_requires=REQUIREMENTS,
+    install_requires=[
+        "beautifulsoup4>=4.6.0",
+        "click>=7.0",
+        "colorama>=0.3.9",
+        "requests>=2.18.0",
+    ],
     keywords="cli redflagdeals",
     license="Apache License, Version 2.0",
     long_description=long_description,
